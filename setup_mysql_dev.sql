c73cd8f904e34041b8cf9fc2fdc82c898174210d
Oussama El aouzari <125877426+gtpoint@users.noreply.github.com>
2023-09-17 23:03:40 +0200

    Update setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,14 +1,14 @@
--- Create the database if it doesn't exist
+-- Create or Replace the database 'hbnb_dev_db'
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
--- Create the user if it doesn't exist and set the password
-CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Create or Replace the user 'hbnb_dev' with password 'hbnb_dev_pwd' in localhost
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
--- Grant all privileges on the database to the user
+-- Grant all privileges on 'hbnb_dev_db' to 'hbnb_dev'
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
--- Grant SELECT privilege on performance_schema to the user
+-- Grant SELECT privilege on 'performance_schema' to 'hbnb_dev'
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
--- Flush privileges to apply the changes
+-- Flush privileges to apply changes
 FLUSH PRIVILEGES;
