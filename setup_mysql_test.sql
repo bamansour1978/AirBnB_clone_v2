6b78982072321ccfbbf473c329d006a1574030d5
bamansour1978 <karatman@gmail.com>
2023-09-19 22:54:22 +0100

    Update

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -6,5 +6,5 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
 
--- Flush privileges to apply changes
+-- Flush privileges to apply changes :
 FLUSH PRIVILEGES;
