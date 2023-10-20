c35b8ddf7bd6901bf2013899f808175cda893d19
bamansour1978 <karatman@gmail.com>
2023-10-20 22:36:05 +0100

    2. C is fun!

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+ A script starts a Flask web application:
+    web application must be listening on 0.0.0.0, port 5000
+    Routes:
+        /: display “Hello HBNB!”
+        /hbnb: display "HBNB"
+        /c/<text>:  display “C ” followed by the value of the text variable
+                    (replace underscore _ symbols with a space)
+You must use the option strict_slashes=False
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
