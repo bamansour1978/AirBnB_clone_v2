b53d893b130768a40e290d4566c0f46ee74b6438
bamansour1978 <karatman@gmail.com>
2023-10-21 19:47:08 +0100

    Fix error

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -9,13 +9,23 @@ from flask import render_template
 app = Flask(__name__)
 
 
+@app.route("/states", strict_slashes=False)
+def states():
+    """Displays an HTML page with a list of all States.
+
+    States are sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("9-states.html", state=states)
+
+
 @app.route("/states/<id>", strict_slashes=False)
 def states_id(id):
     """
     /states_id: page Html with id
     """
     for state in storage.all("State").values():
-        if states_id == id:
+        if state.id == id:
             return render_template("9-states.html", state=state)
     return render_template("9-states.html")
 
